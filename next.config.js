8e27794253a1e6af6ee17c3f5dff7574b9162d84
ParesEntre <paresentre@gmail.com>
2026-04-03 17:03:29 -0400

    Fix: next.config simplificado

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-cat > next.config.js << 'EOF'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'jetjlftghxhzrbmlzjeh.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-    ],
   },
 }
 module.exports = nextConfig
-EOF
