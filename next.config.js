60ee4b2ee820ecd1daac15b1c92ee5a3cb5ef987
ParesEntre <paresentre@gmail.com>
2026-04-03 16:45:44 -0400

    Fix: next.config.js solo con Supabase

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+cat > next.config.js << 'EOF'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,11 +10,8 @@ const nextConfig = {
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'www.entrepares.cloud',
-      },
     ],
   },
 }
 module.exports = nextConfig
+EOF
